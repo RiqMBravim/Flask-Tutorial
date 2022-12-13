5ad23859f379f518b2d30bafddf53c9f9b48c13a
RiqMBravim <henriquembravim@gmail.com>
2022-12-13 13:31:56 -0300

    test coverage (erro na função test_hello dentro de test_factory.py)

--- flask-tutorial/tests/test_factory.py
+++ flask-tutorial/tests/test_factory.py
@@ -5,8 +5,7 @@ def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
 
-try:
-    def test_hello(client):
-        response = client.get('/hello')
-        assert response.data == b'Hello, World!'
-except
+
+def test_hello(client):
+    response = client.get('/hello')
+    assert response.data == b'Hello, World!'
